980d53cf7b0c574530bd757bf5af8d340ab5bef1
Allen Chou <f1470891079@gmail.com>
2023-09-26 19:30:10 +0800

    Try other APIs.

---
+++ go-red/demo.go
@@ -0,0 +1,17 @@
+package go_red
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func demo() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":8080")
+}
--- go-red/main.go
+++ go-red/main.go
@@ -1,4 +1,4 @@
-package main
+package go_red
 
 import (
 	"github.com/gin-gonic/gin"
